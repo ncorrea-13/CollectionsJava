f2d3011b9d2358a58b16765491dfdeb0d83b5d46
Nicolás <nicolascorrea1910@gmail.com>
2022-03-15 19:42:16 -0300

    0.1

--- Ej1/src/ej1/Ej1.java
+++ Ej1/src/ej1/Ej1.java
@@ -1,11 +1,11 @@
-
 package ej1;
 
+import java.util.ArrayList;
 
 public class Ej1 {
 
     public static void main(String[] args) {
-
+        ArrayList<String> Messi = new ArrayList();
     }
 
 }
