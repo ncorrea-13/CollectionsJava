011d45d24df1d84607b9bbd8c701fa7f83232395
Nicolás <nicolascorrea1910@gmail.com>
2022-03-16 20:52:03 -0300

    0.1.2

--- Ej1/src/ej1/Ej1.java
+++ Ej1/src/ej1/Ej1.java
@@ -1,20 +1,12 @@
 package ej1;
 
 import Objetos.Servicio;
-import java.util.ArrayList;
 
 public class Ej1 {
 
     public static void main(String[] args) {
-        ArrayList<String> Lista = new ArrayList();
         Servicio service = new Servicio();
-        int i = 1;
-        while (i == 1) {
-            Lista.add(service.crearRaza());
-            i = service.seguirAgregando();
-        }
-
-        System.out.println(Lista.toString());
+        service.controlServicio();
     }
 
 }
